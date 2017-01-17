e5385ac138bb7aa04d9d4f99cf10d77ee89c3323
TsutsumiShogo <threepipesatos@gmail.com>
2017-01-17 14:43:45 +0900

    problemDB専用スクリプト追加

---
+++ problemDB.py
@@ -0,0 +1,52 @@
+import Database as db
+import requests as req
+import json
+
+base_url = 'http://codeforces.com/'
+url = base_url + 'api/'
+
+class ProblemDB:
+    table_name = 'ProblemTable'
+
+    def __init__(self):
+        self.con = db.Connector()
+
+    def initTables(self, drop_table=False):
+        prob_table = {
+            'id': 'VARCHAR(20)',
+            'contestId': 'INT(10)',
+            'prob_index': 'VARCHAR(5)',
+            'points': 'INT(5)',
+        }
+        self.con.createTable(self.table_name, prob_table, primary_key='id', drop=drop_table)
+
+    def addProblem(self, prob_json):
+        points = 0
+        if 'points' in prob_json:
+            points = int(prob_json['points'])
+        data = {
+            'id': str(prob_json['contestId']) + prob_json['index'],
+            'contestId': prob_json['contestId'],
+            'prob_index': prob_json['index'],
+            'points': points,
+        }
+        self.con.insert(data, self.table_name)
+
+    def close(self):
+        self.con.close()
+
+
+def getData(api):
+    request = req.get(url+api)
+    return request.json()
+
+
+if __name__ == '__main__':
+    db = ProblemDB()
+    db.initTables()
+    print('fin set db')
+    data = getData('problemset.problems')
+    print('fin get data')
+    for d in data['result']['problems']:
+        db.addProblem(d)
+    db.close()
