eaeac1a76a85b3a272437970c9f5d2e3588c3ccf
TsutsumiShogo <threepipesatos@gmail.com>
2017-06-07 05:10:20 +0900

    EditDistPlot: pid -> label のマッピングクラスを渡すことで，問題を分類して統計を取れるように

--- uploads/EditDistancePlot.py
+++ uploads/EditDistancePlot.py
@@ -6,6 +6,8 @@ import numpy as np
 from boxplot import boxplot
 from DistanceAnalyzer import rating_split, stat_types, stat_method
 
+from acceptanceDB import AcceptanceDB
+from problemDB import ProblemDB
 from EditDistanceStatistics import EditDistanceStatisticsDB
 
 def plot_statistics(data_list, path, file_name, ylim=None):
@@ -87,9 +89,84 @@ def statistics_each_problem():
         plot_statistics(stat, norm_path, prob_id + '.png')
 
 
-def statistics_score():
-    pass
+class ScoreLabel:
+    def __init__(self):
+        self.score_dict = self.get_score_dict()
+
+    def get_score_dict(self):
+        valid_score = [(x + 1) * 500 for x in range(6)]
+        pdb = ProblemDB()
+        prob_dict = {}
+        for prob in pdb.select(['problem_id', 'points']):
+            sc = prob['points']
+            if sc not in valid_score:
+                continue
+            prob_dict[prob['problem_id']] = sc
+        return prob_dict
+
+    def to_label(self, pid):
+        if pid not in self.score_dict:
+            return None
+        return self.score_dict[pid]
+
+
+class AccLabel:
+    def __init__(self):
+        self.acc_split = [
+            0.15, 0.25, 0.35, 0.45, 0.55, 0.8, 1.0
+        ]
+        self.acc_dict = self.get_acc_dict()
+
+    def get_acc_dict(self):
+        acdb = AcceptanceDB()
+        acc_dict = {}
+        for prob in acdb.select(['problem_id', 'acceptance_rate']):
+            acc = prob['acceptance_rate']
+            acc_dict[prob['problem_id']] = acc
+        return acc_dict
+
+    def to_label(self, pid):
+        if pid not in self.acc_dict:
+            return None
+        acc = self.acc_dict[pid]
+        for border in self.acc_split:
+            if acc <= border:
+                return border
+        return 1.0
+
+
+def plot_editdistance_statistics(label_matcher, name):
+    edb = EditDistanceStatisticsDB()
+    save_path = './editdistance_statistics/by_%s/' % name
+    raw_path = save_path + 'raw/'
+    norm_path = save_path + 'normalized/'
+    prob_stat_labeled = {}
+    prob_stat = {}
+    for prob_id, stat in load_prob_stat(edb):
+        prob_stat[prob_id] = stat
+        label = label_matcher.to_label(prob_id)
+        if not label:
+            continue
+        if label not in prob_stat_labeled:
+            prob_stat_labeled[label] = []
+        prob_stat_labeled[label] += stat
+
+    for label, stat in prob_stat_labeled.items():
+        plot_statistics(stat, raw_path, str(label) + '.png')
+
+    normalize_dataset(prob_stat, edb)
+    prob_stat_labeled = {}
+    for prob_id, stat in prob_stat.items():
+        label = label_matcher.to_label(prob_id)
+        if not label:
+            continue
+        if label not in prob_stat_labeled:
+            prob_stat_labeled[label] = []
+        prob_stat_labeled[label] += stat
+
+    for label, stat in prob_stat_labeled.items():
+        plot_statistics(stat, norm_path, str(label) + '.png')
 
 
 if __name__ == '__main__':
-    statistics_each_problem()
+    plot_editdistance_statistics(AccLabel(), 'acc')
