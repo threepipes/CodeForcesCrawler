eb59c30e711f5d973cf1f446babc9a30351971d8
threepipes_s <threepipesatos@gmail.com>
2017-06-12 22:27:30 +0900

    差分配列DB構築スクリプト記述．未テスト

--- uploads/submissionHistoryDB.py
+++ uploads/submissionHistoryDB.py
@@ -5,12 +5,14 @@ from fileDB import FileDB
 from Analyzer import CAExecuter
 
 
-class SubmissionHistory(Database):
+class SubmissionHistoryDB(Database):
     table_name = 'SubmissionDistance'
     key = 'file_name'
     column = [
         'file_name',
         'next_file',
+        # (user, problem)に対する何番目の提出か
+        'submission_index',
         'levenshtein_distance',
         'add_node',
         'delete_node',
@@ -19,6 +21,7 @@ class SubmissionHistory(Database):
     data_table = {
         'file_name': 'VARCHAR(50)',
         'next_file': 'VARCHAR(50)',
+        'submission_index': 'INT(3)',
         'levenshtein_distance': 'INT(5)',
         'add_node': 'INT(4)',
         'delete_node': 'INT(4)',
@@ -29,7 +32,7 @@ class SubmissionHistory(Database):
         super().__init__(self.table_name, self.key, self.column, self.data_table)
 
 
-def store_gumtree_result(user_result):
+def parse_gumtree_result(user_result):
     """
     CodeAnalyzerのgumtreediffによる結果をパースする
     各提出について，(add, delete, update)のタプルに変換した結果を返す
@@ -47,14 +50,73 @@ def store_gumtree_result(user_result):
     return result
 
 
+def store_user_result(hist_list, result_gt, result_lv, sdb):
+    """
+    取得した差分配列をDBに格納
+    hist_list: 提出履歴のリスト
+    result_gt: hist_listに対するgumtreeによる差分
+    result_lv: hist_listに対するlevenshteinによる差分
+    全部同じ形(ただしhist_listよりresultは1列少ない)を前提
+    """
+    for i in range(len(hist_list)):
+        for j in range(len(hist_list[i])):
+            file_name = hist_list[i][j].split('/')[-1]
+            if j < len(hist_list[i]) - 1:
+                next_file = hist_list[i][j + 1].split('/')[-1]
+                leven_dist = result_lv[i][j]
+                gumtree_data = result_gt[i][j]
+                add = gumtree_data[0]
+                delete = gumtree_data[1]
+                update = gumtree_data[2]
+            else:
+                next_file = '-'
+                leven_dist = 0
+                add = 0
+                delete = 0
+                update = 0
+            submission_index = j + 1
+            sdb.insert({
+                'file_name': file_name,
+                'next_file': next_file,
+                'submission_index': submission_index,
+                'levenshtein_distance': leven_dist,
+                'add_node': add,
+                'delete_node': delete,
+                'update_node': update
+            })
+    sdb.commit()
+
+
+def store_user_statistics( \
+        user_name: str, \
+        fdb: FileDB, \
+        analyzer: CAExecuter, \
+        sdb: SubmissionHistoryDB):
+    """
+    あるuserの提出履歴に対する差分配列を取得し，
+    DBに格納する．
+    """
+    pid_list, hist_list = get_history_list(user_name, fdb, 'src/')
+    analyzer.set_command({'task': 'dist', 'method': 'gumtree'})
+    analyzer.write_list(hist_list)
+    analyzer.execute()
+    result_gt = analyzer.read_result()
+    result_gt = parse_gumtree_result(result_gt)
+    analyzer.set_command({'task': 'dist', 'method': 'leven'})
+    analyzer.execute()
+    result_lv = analyzer.read_result()
+    store_user_result(hist_list, result_gt, result_lv, sdb)
+
+
 def init_db():
+    """
+    UserDB, FileDB, sourceから
+    SubmissionHistoryDBを構成する
+    """
     udb = UserDB()
     fdb = FileDB()
+    sdb = SubmissionHistoryDB()
+    sdb.init_table()
     analyzer = CAExecuter()
     for user in udb.select():
-        user_name = user['user_name']
-        pid_list, hist_list = get_history_list(user_name, fdb, 'src/')
-        analyzer.set_command({'task': 'dist', 'method': 'gumtree'})
-        analyzer.write_list(hist_list)
-        analyzer.execute()
-        result = analyzer.read_result()
+        store_user_statistics(user['user_name'], fdb, analyzer, sdb)
