d27a0db2daa635a71e0cedaaba3e95bfafa0ade3
threepipes <threepipesatos@gmail.com>
2017-06-14 20:43:19 +0900

    ファイルの抽出条件を変更(CEを除外)

--- uploads/fileDB_util.py
+++ uploads/fileDB_util.py
@@ -2,5 +2,6 @@ lang_list = ['GNU C++14', 'GNU C++11', 'GNU C++']
 lang_select = '(%s)' % ' or '.join(["lang='%s'" % lang for lang in lang_list])
 base_selection = [
     lang_select,
-    'during_competition=1'
+    # 'during_competition=1',
+    "verdict!='COMPILATION_ERROR'"
 ]
