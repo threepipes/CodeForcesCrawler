09e8d931feb20c11c7c10ea73f0589df3d078324
threepipes <threepipesatos@gmail.com>
2017-06-16 16:14:44 +0900

    ProblemEditDistanceDBを大幅改良

--- uploads/EditDistanceStatistics.py
+++ uploads/EditDistanceStatistics.py
@@ -8,33 +8,33 @@ from fileDB import FileDB
 from userDB import UserDB
 from DistanceAnalyzer import analyze_user
 from Database import Database
+from submissionHistoryDB import SubmissionHistoryDB
+
+
+col_list = [
+    'levenshtein_distance',
+    'add_node',
+    'delete_node',
+    'update_node',
+    'move_node',
+    'node_sum',
+]
+
+def_type = [
+    ('min', 'INT'),
+    ('max', 'INT'),
+    ('mean', 'DOUBLE'),
+    ('median', 'INT'),
+    ('variance', 'DOUBLE'),
+]
 
 
 class EditDistanceStatisticsDB(Database):
     table_name = 'ProblemEditDistance'
     key = 'problem_id'
-    column = [
-        key,
-        'max',
-        'min',
-        'mean',
-        'median',
-        'variance',
-        'solver_rating_mean',
-        'solver_rating_median',
-    ]
-    data_table = {
-        key: 'VARCHAR(20)',
-        'max': 'INT',
-        'min': 'INT',
-        'mean': 'DOUBLE',
-        'median': 'INT',
-        'variance': 'DOUBLE',
-        'solver_rating_mean': 'DOUBLE',
-        'solver_rating_median': 'INT',
-    }
 
     def __init__(self):
+        self.init_column()
         super(EditDistanceStatisticsDB, self).__init__(
             self.table_name,
             self.key,
@@ -42,36 +42,25 @@ class EditDistanceStatisticsDB(Database):
             self.data_table
         )
 
-
-def get_prob_data():
-    udb = UserDB()
-    user_list = udb.select(col=['user_name', 'rating'])
-    udb.close()
-    fdb = FileDB()
-
-    prob_stat = {}
-    for i, userdata in enumerate(user_list):
-        """
-        まず正規化のための中央値を計算する
-        user_resultはuserdataに退避
-        """
-        name = userdata['user_name']
-        user_result = analyze_user(name, fdb)
-        userdata['result'] = user_result
-        rating = userdata['rating']
-        print(name, i)
-
-        for sub_data in user_result:
-            pid = sub_data['pid']
-            # for dist in sub_data['statistics']:
-            if pid not in prob_stat:
-                prob_stat[pid] = []
-            stat = sub_data['statistics']
-            prob_stat[pid].append({
-                'rating': rating,
-                'diffs': stat
-            })
-    return prob_stat
+    def init_column(self):
+        column = [self.key]
+        data_table = {
+            self.key: 'VARCHAR(20)',
+            'solver_rating_mean': 'DOUBLE',
+            'solver_rating_median': 'INT',
+        }
+        for col in col_list:
+            for tp in def_type:
+                colname = col + '_' + tp[0]
+                coltype = tp[1]
+                column.append(colname)
+                data_table[colname] = coltype
+        column += [
+            'solver_rating_mean',
+            'solver_rating_median',
+        ]
+        self.column = column
+        self.data_table = data_table
 
 
 def get_statistics(data: list):
@@ -92,63 +81,35 @@ def get_statistics(data: list):
     }, ratings
 
 
-def plot_rating_histogram(save_path, rating_list):
-    """
-    500スタートで100刻みで3000までプロット
-    """
-    plt.figure(figsize=(20, 20))
-    plt.hist(rating_list, bins=100, range=(500, 3000))
-    plt.ylim((0, 200))
-    plt.savefig(save_path)
-    plt.close()
+def get_prob_list(json_path):
+    prob_list = []
+    for file_name in os.listdir(json_path):
+        prob_list.append(file_name.split('.')[0])
+    return prob_list
 
 
-def dump_json(save_path, dump_data):
-    """
-    編集距離等の統計情報をjsonファイルとして保存
-    """
-    with open(save_path, 'w') as f:
-        json.dump(dump_data, f)
-
-
-def main(edb):
-    save_dir = 'editdistance_statistics/'
-    json_dir = save_dir + 'json/'
-    plot_dir = save_dir + 'rating/'
-    if not os.path.exists(save_dir):
-        os.mkdir(save_dir)
-        os.mkdir(json_dir)
-        os.mkdir(plot_dir)
-    """
-    編集距離リストなどのjsonデータ -> save_dir/json/prob_id
-    回答者レーティングの度数分布プロット -> save_dir/rating/prob_id
-    """
-    prob_stat = get_prob_data()
-    for pid, data in prob_stat.items():
-        statistics, ratings = get_statistics(data)
-        if not statistics:
-            continue
-        print('prob: ' + str(pid))
-        statistics['problem_id'] = pid
-        rating_list = np.array(ratings)
-        statistics['solver_rating_mean'] = float(np.mean(rating_list))
-        statistics['solver_rating_median'] = int(np.median(rating_list))
+base_path = './editdistance_statistics/json/'
+def set_db(edb: EditDistanceStatisticsDB):
+    column_list = col_list[:-1]
+    prob_list = get_prob_list(base_path + column_list[0])
+    for prob_id in prob_list:
+        print(prob_id)
+        ratings = None
+        statistics = {}
+        for col in column_list:
+            path = base_path + col + '/%s.json' % prob_id
+            with open(path, encoding='utf-8') as f:
+                data = json.load(f)
+            stat, rat = get_statistics(data)
+            if ratings is None:
+                ratings = rat
+            for tp in stat.keys():
+                statistics[col + '_' + tp] = stat[tp]
+        statistics['problem_id'] = prob_id
+        ratings = np.array(ratings)
+        statistics['solver_rating_mean'] = float(np.mean(ratings))
+        statistics['solver_rating_median'] = int(np.median(ratings))
         edb.insert(statistics)
-        edb.commit()
-        plot_rating_histogram(plot_dir + str(pid) + '.png', rating_list)
-        dump_json(json_dir + str(pid) + '.json', data)
-
-
-def rating_statistics():
-    from EditDistancePlot import load_prob_stat
-    save_dir = 'editdistance_statistics/'
-    plot_dir = save_dir + 'rating/'
-    edb = EditDistanceStatisticsDB()
-    prob_stat = load_prob_stat(edb, False)
-    for pid, data in prob_stat:
-        stat, ratings = get_statistics(data)
-        rating_list = np.array(ratings)
-        plot_rating_histogram(plot_dir + str(pid) + '.png', rating_list)
 
 
 def init_db():
@@ -162,4 +123,5 @@ def init_db():
 """
 
 if __name__ == '__main__':
-    rating_statistics()
+    edb = init_db()
+    set_db(edb)
