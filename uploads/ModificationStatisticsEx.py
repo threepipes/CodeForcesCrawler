5aed34cde5e829d43df6d7066a0b1bccc95c4601
threepipes <threepipesatos@gmail.com>
2017-08-02 03:18:29 +0900

    DataCollector関連を削除

--- uploads/ModificationStatisticsEx.py
+++ uploads/ModificationStatisticsEx.py
@@ -119,61 +119,6 @@ def parallel_collect():
     return ms_fixdata
 
 
-class DataCollector:
-    def __init__(
-                self,
-                sdb: SubmissionHistoryDB,
-                fdb: FileDB,
-                pdb: ProblemDB,
-                udb: UserDB,
-                mdb: ModificationDB,
-                classifier: FixClassifier
-            ):
-        self.sdb = sdb
-        self.fdb = fdb
-        self.pdb = pdb
-        self.mdb = mdb
-        self.udb = udb
-        self.classifier = classifier
-
-    def collect_by_user(self, user_name, prob_name):
-        """
-        該当ユーザーの該当問題における修正を集計する
-        どの位置でどの種類(addなど)の修正が何回起こったかを集計する
-        """
-        where = {'user_name': user_name, 'problem_id': prob_name}
-        div_name = None
-        multiset = {}
-        for fix_data in self.mdb.select(where=where):
-            data_dict, data_name = self.classifier(fix_data)
-            if div_name is None:
-                div_name = data_dict[problem_div] + '_' + data_dict[user_div]
-                if multiset3 is not None:
-                    if div_name not in multiset3:
-                        multiset3[div_name] = {}
-                    multiset = multiset3[div_name]
-            add_multiset2(multiset, data_dict[tag_div], data_dict[type_div])
-        return multiset, div_name
-
-    def collect_by_prob(self, prob_name, multiset3):
-        """
-        問題ごとの集計を行う
-        問題における中央値や平均の算出も行う
-        並列実行のため，独立に実行できるようにしたい
-        """
-        where = {'problem_id': prob_name}
-        for user in self.fdb.select(col=['user_name'], distinct=True, where=where):
-            user_name = user['user_name']
-            ms_fixdata, div_name = self.collect_by_user(user_name, prob_name)
-            # multiset 
-
-    def collect(self):
-        database = {}
-        for prob in self.pdb.select():
-            prob_id = prob['problem_id']
-            self.collect_by_prob(prob_id, database)
-
-
 def get_type_order():
     order = [
         {'name': 'INS', 'color': (1, 0, 0)},
@@ -182,30 +127,6 @@ def get_type_order():
         {'name': 'MOV', 'color': (0, 0, 1)},
     ]
     return order
-
-
-def generate_data_collector():
-    adb = AcceptanceDB()
-    udb = UserDB()
-    collector = DataCollector(
-        SubmissionHistoryDB(),
-        FileDB(),
-        ProblemDB(),
-        UserDB(),
-        ModificationDB(),
-        build_mod_classifier_ex(adb, udb)
-    )
-    return collector
-
-
-def test_collect_by_user():
-    collector = generate_data_collector()
-    collect_data, div_name = collector.collect_by_user('-77-', '707B')
-
-    print(div_name)
-    print(collect_data)
-
-    barh_plot_m2(collect_data, get_type_order(), div_name, path=('plot_example/%s.png' % div_name))
 
 
 def build_mod_classifier_ex(adb: AcceptanceDB, udb: UserDB):
@@ -217,19 +138,6 @@ def build_mod_classifier_ex(adb: AcceptanceDB, udb: UserDB):
         clsf_prob, clsf_user, clsf_tag, clsf_type
     ])
     return classifier
-
-
-def test_collect_by_prob():
-    prob_id = '715A'
-    data = {}
-    collector = generate_data_collector()
-    collector.collect_by_prob(prob_id, data)
-    for name, data_dict in data.items():
-        print(name)
-        barh_plot_m2(
-            data_dict, get_type_order(),
-            title=name, path=('plot_example/%s.png' % name)
-        )
 
 
 def collect_plot_prob_fix():
